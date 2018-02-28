a93fcb9627e31bf9741b4497031f5b793d9907ec
ScottGrogin <israelgrogin@gmail.com>
2018-02-27 22:44:04 -0800

    Got rid of unused layouts and changed the main act's theme to matach the others

--- app/src/main/java/com/aei/training/MainActivity.java
+++ app/src/main/java/com/aei/training/MainActivity.java
@@ -2,17 +2,8 @@ package com.aei.training;
 
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     private CardDisplayLayout cardDisplayLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
 
         //create a new card layout
