8843cbd49acda7b43149653b01df84d4690ca72b
Adam01010101F <watsaname1999@gmail.com>
2018-02-28 11:48:49 -0800

    Fixed URL

--- app/src/main/java/com/aei/training/MetroAPI/mGrabsEst.kt
+++ app/src/main/java/com/aei/training/MetroAPI/mGrabsEst.kt
@@ -10,14 +10,14 @@ import retrofit2.http.GET
  * Created by Adam F on 2/24/2018.
  */
 
-
+//getStop
 //TODO: FIX UrL && rm hardcode
 interface mGrabEst{
-    @GET("agencies/lametro-rail/stops/80101/predictions/")       //Eventually change 80101 to $stop_id
+    @GET("agencies/lametro-rail/routes/801/stops/80101/predictions/")       //Eventually change 80101 to $stop_id
     fun getEst() : Call<EstimateList>
 }
 
-//fun is functioninterface getLine
+//fun is function interface getLine
 interface mGetLine {
     @GET("agencies/lametro-rail")
     fun getLine() : Call<TrainLine>
