9704c13af963e3005388b5a4dbc1212feceb0cef
Adam U Flores <watsaname1999@gmail.com>
2018-03-20 14:43:14 -0700

    Added Facebook Login Added FB SDK  - Edited Gradle Files  - Edited Android Manifest Edited Activity_Main.xml button and edit text id's

--- app/src/main/java/com/aei/training/MainActivity.java
+++ app/src/main/java/com/aei/training/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,7 +30,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -38,16 +48,45 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class MainActivity extends AppCompatActivity {
-
-
+    private LoginButton fbookButton;
+    private EditText email;
+    private CallbackManager cbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
         // Set up the login form.
+        cbManager = CallbackManager.Factory.create();
+        email = findViewById(R.id.emailField);
+        fbookButton = findViewById(R.id.fbButton);
+
+        fbookButton.setReadPermissions(Arrays.asList(email.getText().toString()));
+
+        // Callback registration
+        fbookButton.registerCallback(cbManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e("onError: Login", exception.toString());
+            }
+        });
+
 
     }
 
+
+    
 }
 
