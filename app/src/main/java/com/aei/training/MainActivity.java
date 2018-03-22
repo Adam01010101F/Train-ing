85492dd87e88324d718ef859273c5082e9febc0a
Adam U Flores <watsaname1999@gmail.com>
2018-03-21 23:29:07 -0700

    Fixed Facebook Login Should move screens on login Added Firebase integration of FB

--- app/src/main/java/com/aei/training/MainActivity.java
+++ app/src/main/java/com/aei/training/MainActivity.java
@@ -10,7 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -18,8 +20,11 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -143,16 +148,26 @@ public class MainActivity extends AppCompatActivity {
                 fbookButton.registerCallback(cbManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        Log.d("onSuccess", "Its getting here!");
+                        facebookLoginToken(loginResult.getAccessToken());
+                        Intent intent = new Intent(MainActivity.this, LineSelectActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
 
                     }
 
                     @Override
                     public void onCancel() {
-                        // App code
+                        Toast.makeText(getApplicationContext(), "Bad Facebook Credentials", Toast.LENGTH_LONG);
+                        Log.d("onCancel", "Its getting here!");
+
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
+                        Log.d("onError", "Its getting here!");
+
                         Log.e("onError: Login", exception.toString());
                     }
                 });
@@ -161,6 +176,49 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+        if(currentUser != null){
+
+        }
+    }
+
+    private void facebookLoginToken(AccessToken token) {
+        Log.d("FacebookLogin", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("FacebookLogin", "signInWithCredential:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, LineSelectActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("FacebookLogin", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        cbManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
