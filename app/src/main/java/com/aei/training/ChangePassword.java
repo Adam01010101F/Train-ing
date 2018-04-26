f24bffb344c2913d7fda2b500972613ebd0154c0
Adam U Flores <watsaname1999@gmail.com>
2018-04-25 21:27:02 -0700

    "Finished" Change Password Functionality
    KNOWN BUG: If JWT is expired, password can't be changed.
    
    CURRENT WORKAROUND: Logout before changing passwords.

--- app/src/main/java/com/aei/training/ChangePassword.java
+++ app/src/main/java/com/aei/training/ChangePassword.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,22 +26,36 @@ public class ChangePassword extends AppCompatActivity{
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        fireAuth.getInstance();
+        //Setting Firebase vars
+        fireAuth = FirebaseAuth.getInstance();
+        user = fireAuth.getCurrentUser();
+        //Setting UI cars
         setContentView(R.layout.activity_change_password);
         newPass =(EditText) findViewById(R.id.passField);
         confPass = (EditText) findViewById(R.id.confPassField);
         changePass = (Button) findViewById(R.id.chngPassButton);
 
+
+
         changePass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!changePass.getText().toString().isEmpty()){
+                if(!newPass.getText().toString().isEmpty()
+                        && newPass.getText().toString().equals(confPass.getText().toString())){
                     changePassword(changePass.getText().toString());
+//                    Log.d("EDITFIELDS:", newPass.getText().toString() + confPass.getText().toString());
+                }
+                else{
+                    if(newPass.getText().toString().isEmpty()){
+                        popToast("Enter a password.");
+                    } else{
+                        popToast("Passwords do not match.");
+                    }
                 }
             }
         });
     }
-
+//  TODO: KNOWN BUG - If JWT is expired, password can't be changed.
     protected void changePassword(String newPassword){
         user.updatePassword(newPassword)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -49,9 +64,12 @@ public class ChangePassword extends AppCompatActivity{
                         if (task.isSuccessful()) {
                             Log.d("PsChg", "User password updated.");
                             verifyEmail();
+                            Toast toast = Toast.makeText(getApplication().getBaseContext(), "Password Changed"
+                                    , Toast.LENGTH_LONG);
+                            toast.show();
                         }
                         else{
-                            Log.d("PsChg", "User password not updated.");
+                            Log.d("PsChg", task.getException().toString());
                         }
                     }
                 });
@@ -67,5 +85,11 @@ public class ChangePassword extends AppCompatActivity{
                         }
                     }
                 });
+    }
+
+    protected void popToast(String msg){
+        Toast toast = Toast.makeText(getApplication().getBaseContext(), msg,
+                Toast.LENGTH_LONG);
+        toast.show();
     }
 }
