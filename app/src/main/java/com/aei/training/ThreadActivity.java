7c462cb9cb346982a0fe5deeeeb85ae09fc5fa41
Adam U Flores <watsaname1999@gmail.com>
2018-05-16 08:45:05 -0700

    Current work
    meaning nothing.

--- app/src/main/java/com/aei/training/ThreadActivity.java
+++ app/src/main/java/com/aei/training/ThreadActivity.java
@@ -9,19 +9,27 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+
+import io.grpc.Server;
 
 public class ThreadActivity extends AppCompatActivity{
     private FirebaseAuth fAuth;
     private FirebaseUser fUser;
     private FirebaseFirestore fStore;
     private Button genThread;
+    private Button queryButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView();
 //        genThread = findViewById(R.id.genThread);
+//        String line = getIntent().getStringExtra("ID");
         fAuth = FirebaseAuth.getInstance();
         fUser = fAuth.getCurrentUser();
         fStore =  FirebaseFirestore.getInstance();
@@ -38,13 +46,29 @@ public class ThreadActivity extends AppCompatActivity{
                 generateTempThread();
             }
         });
+        queryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                queryPosts();
+            }
+        });
     }
 
     private void generateTempThread(){
-        fStore.collection("Thread").add()
+        HashMap<String, Object> tThread = new HashMap<>();
+        tThread.put("Title", "This is a test");
+        tThread.put("Text", "So I need to create a thread, so here is the thread body.");
+        String tester = "Test";
+        fStore.collection("Thread").document(String.valueOf(new String("Test")
+                .hashCode())).set(tThread);
+        DocumentReference tDoc = new 
+        fStore.collection("Post").document(String.valueOf
     }
-    /** Append to an existing document
-     * db.collection("cities").document("BJ")
-     *      .set(data, SetOptions.merge());
+    /**
+     * TODO: Retrieve all posts from server.
+     * Find a way to order them(Server Timestamp as posts are created?)
      */
+    private void queryPosts(){
+
+    }
 }
