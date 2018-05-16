93374c8ed2530072518e0c6dd863058c6dae85c7
Adam U Flores <watsaname1999@gmail.com>
2018-05-16 14:55:22 -0700

    Current work Implementing gson to stringify classes for easier storage. Will Store messages as a ArrayList of messages to keep order.

---
+++ app/src/main/java/com/aei/training/Objects/Post.kt
@@ -0,0 +1,5 @@
+package com.aei.training.Objects
+
+import java.io.Serializable
+
+data class Post(val User: String, val Comment: String) : Serializable {}
--- app/src/main/java/com/aei/training/ThreadActivity.java
+++ app/src/main/java/com/aei/training/ThreadActivity.java
@@ -1,17 +1,28 @@
 package com.aei.training;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.gson.*;
 
 import java.util.HashMap;
 
@@ -23,21 +34,19 @@ public class ThreadActivity extends AppCompatActivity{
     private FirebaseFirestore fStore;
     private Button genThread;
     private Button queryButton;
+    private Button postButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView();
+        setContentView(R.layout.activity_thread_posts);
+//        String threadId= getIntent().getStringExtra("ID");
+        String threadId = "QDQenIbvuihORLoawCbE";
 //        genThread = findViewById(R.id.genThread);
-//        String line = getIntent().getStringExtra("ID");
-        fAuth = FirebaseAuth.getInstance();
-        fUser = fAuth.getCurrentUser();
-        fStore =  FirebaseFirestore.getInstance();
+        postButton = findViewById(R.id.AddPost);
+//        queryButton = findViewById(R);
 
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setPersistenceEnabled(true)
-                .build();
-        fStore.setFirestoreSettings(settings);
+        instaFire();
 
         //Code for testing
         genThread.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +61,13 @@ public class ThreadActivity extends AppCompatActivity{
                 queryPosts();
             }
         });
+        postButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                showPostBox();
+            }
+        });
+        getQuery(threadId);
     }
 
     private void generateTempThread(){
@@ -61,14 +77,75 @@ public class ThreadActivity extends AppCompatActivity{
         String tester = "Test";
         fStore.collection("Thread").document(String.valueOf(new String("Test")
                 .hashCode())).set(tThread);
-        DocumentReference tDoc = new 
-        fStore.collection("Post").document(String.valueOf
+//        DocumentReference tDoc = new
+//        fStore.collection("Post").document(String.valueOf
     }
+
     /**
      * TODO: Retrieve all posts from server.
      * Find a way to order them(Server Timestamp as posts are created?)
      */
     private void queryPosts(){
 
+    }
+
+    public void instaFire(){
+        fAuth = FirebaseAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+        fStore =  FirebaseFirestore.getInstance();
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        fStore.setFirestoreSettings(settings);
+    }
+
+    public void getQuery(String id){
+
+    }
+
+    public void showPostBox(){
+        final  AlertDialog.Builder alertDialog = new AlertDialog.Builder(ThreadActivity.this);
+        alertDialog.setTitle("Add Post");
+        alertDialog.setMessage("");
+
+        final EditText input = new EditText(ThreadActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("Post",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                String userIn = input.getText().toString();
+
+//                if(userIn.equals(EmailFire)) {
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Posting...", Toast.LENGTH_SHORT);
+                    toast.show();
+                    try{
+//                        fStore.collection("Post").document(threadId).set
+
+                    }catch (Exception e){
+                        toast = Toast.makeText(getApplicationContext(), "Account was not found", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+//                } else {
+//
+//                    Toast toast = Toast.makeText(getApplicationContext(), "FireBase email was not entered, please click delete again", Toast.LENGTH_LONG);
+//                    toast.show();
+//
+//                }
+
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = alertDialog.create();
+        b.show();
     }
 }
