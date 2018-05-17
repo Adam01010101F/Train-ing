4fd487ab1925fb6fa96052f573f09cd07a3e87b2
Adam U Flores <watsaname1999@gmail.com>
2018-05-17 13:32:54 -0700

    Fixed merge conflicts.

--- app/src/main/java/com/aei/training/PostActivity.java
+++ app/src/main/java/com/aei/training/PostActivity.java
@@ -311,6 +311,10 @@ public class PostActivity extends AppCompatActivity {
     public void instaFire() {
         fAuth = FirebaseAuth.getInstance();
         fUser = fAuth.getCurrentUser();
+        if(fUser.getDisplayName().equals(null) || fUser.getDisplayName().equals("")){
+            Intent intent = new Intent(PostActivity.this, EditDisplayInfo.class);
+            startActivity(intent);
+        }
         fStore = FirebaseFirestore.getInstance();
 
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
@@ -322,14 +326,14 @@ public class PostActivity extends AppCompatActivity {
 
     public void showPostBox() {
         final AlertDialog.Builder alertDialog = new AlertDialog.Builder(PostActivity.this);
-        alertDialog.setTitle("Add thread");
+        alertDialog.setTitle("Add Thread");
         alertDialog.setMessage("");
 
         final EditText topicInput = new EditText(PostActivity.this);
         topicInput.setHint("Enter Topic");
         topicInput.setTextColor(0xff000000);
         final EditText comment = new EditText(PostActivity.this);
-        comment.setHint("Enter message");
+        comment.setHint("Enter Topic ");
         comment.setTextColor(0xff000000);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
@@ -350,7 +354,29 @@ public class PostActivity extends AppCompatActivity {
                 toast.show();
                 postThread(userTopic, userInput, displayName, TrainThreadLine);
 
+                //create a thread
 
+         /*           try {
+//                        fStore.collection("Post").document(threadId).set
+
+                    } catch (Exception e) {
+                        toast = Toast.makeText(getApplicationContext(), "Account was not found", Toast.LENGTH_LONG);
+                        toast.show();
+                    }*/
+        /*        try {
+
+
+                } catch (Exception e) {
+                    toast = Toast.makeText(getApplicationContext(), "Account was not found", Toast.LENGTH_LONG);
+                    toast.show();
+                }*/
+
+//                } else {
+//
+//                    Toast toast = Toast.makeText(getApplicationContext(), "FireBase email was not entered, please click delete again", Toast.LENGTH_LONG);
+//                    toast.show();
+//
+//                }
             }
 
         });
@@ -385,6 +411,7 @@ public class PostActivity extends AppCompatActivity {
                     }
                 });
     }
+
     private void postThread(String userTopic, String userInput, String  displayName, String TrainThreadLine)
     {
         Map<String,Object> data =  new HashMap<>();
