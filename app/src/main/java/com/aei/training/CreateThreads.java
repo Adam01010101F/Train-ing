e1afe1bd2d401cb966bccb863211a77164f84069
92Eric <ericaguirre@me.com>
2018-05-16 19:28:18 -0700

    "added two activites to create Threads, one searches for all lines,, one creates even amount for each line"

--- app/src/main/java/com/aei/training/CreateThreads.java
+++ app/src/main/java/com/aei/training/CreateThreads.java
@@ -1,5 +1,6 @@
 package com.aei.training;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -32,5 +33,79 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
 public class CreateThreads extends AppCompatActivity {
+    private FirebaseAuth fAuth;
+    private FirebaseUser fUser;
+    private FirebaseFirestore fStore;
+
+
+
+    private LinearLayout linearLayout;
+   private LinearLayout.LayoutParams params;
+    private static AppCompatActivity appCompatActivity;
+    private Intent intent;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.intent = intent;
+        this.appCompatActivity =appCompatActivity;
+
+//shouldn't need the commented outstuff
+  //      drawerLayout = new DrawerLayout(this.appCompatActivity);
+  //      dParams = new DrawerLayout.LayoutParams( DrawerLayout.LayoutParams.MATCH_PARENT ,  DrawerLayout.LayoutParams.MATCH_PARENT);
+    //    dParams.gravity= Gravity.START;
+     //   navigationView = new NavigationView(this.appCompatActivity);
+       // navigationView.setLayoutParams(dParams);
+       // navigationView.setBackgroundColor(Color.WHITE);
+     //   navigationView.setFitsSystemWindows(true);
+        instaFire();
+    }
+
+
+    public void instaFire() {
+        fAuth = FirebaseAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+        fStore = FirebaseFirestore.getInstance();
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        fStore.setFirestoreSettings(settings);
+    }
+    public void createThreads(String text, int lineColor, boolean clickable,final String id){
+
+        Drawable drawable = ResourcesCompat.getDrawable(this.appCompatActivity.getResources(), R.drawable.square, null);
+        drawable.setColorFilter(lineColor, PorterDuff.Mode.MULTIPLY);
+        TextView textView = new TextView(this.appCompatActivity);
+        textView.setLayoutParams(params);
+        textView.setText(text);
+        textView.setPadding(20,30,0,30);
+        textView.setBackgroundResource(R.drawable.card_shadow);
+        textView.setTypeface(Typeface.create("sans-serif-medium",Typeface.NORMAL));
+        textView.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null);
+        textView.setTextColor(0xff4f4f4f);
+        textView.setTextSize(25);
+        textView.setClickable(clickable);
+        if(clickable){
+        //this will do something else,
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // text gets passed through to the new activity so the new activity can choose
+                    // what to display
+                    intent.putExtra("ID", id);
+                    CreateThreads.appCompatActivity.startActivity(intent);
+
+                }
+            });
+        }
+
+        linearLayout.addView(textView);
+    }
 }
